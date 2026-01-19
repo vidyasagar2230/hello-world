0f16b8978ba3c0de5da6116397eca71ff8318f3b
Vidya Sagar <sagar.nalluri@gmail.com>
2026-01-19 13:40:17 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log("App is running successfully!");
+// app.js
+console.log("-----------------------------------------");
+console.log("🚀 Node.js App is running in GitHub Actions!");
+console.log("📅 Current Date:", new Date().toLocaleDateString());
+console.log("-----------------------------------------");
+
